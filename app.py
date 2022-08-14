cf70e107601e4d62489c13038d28d1a26938937e
Joel Marriott <joel.marriott.uk@gmail.com>
2022-08-14 10:52:29 +0100

    Delete existing tasks added

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
@@ -14,6 +15,7 @@ class Todo(db.Model):
     def __repr__(self):
         'Every time we create a new element return the below'
         return '<Task %r>' %self.id
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -30,6 +32,19 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
